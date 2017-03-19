7bccb8074147242f65bbf12e86fc77316b0d872a
Thomas Nakagawa <thomasnakagawa@gmail.com>
2017-03-19 00:01:45 -0700

    Add more material design and pie chart

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Link, browserHistory } from 'react-router'
-import { Toolbar, ToolbarGroup, FlatButton, Avatar, Subheader, List, ListItem } from 'material-ui'
-import logo from '../../logo.svg'
+import { Paper, Toolbar, ToolbarGroup, FlatButton, Avatar, Subheader, List, ListItem } from 'material-ui'
+import { Doughnut } from 'react-chartjs-2'
+import Logo from '../sharedComponents/logo'
 import './App.css'
 
 const defaultValues = {
   firstName: 'Tim',
   lastName: 'Bo',
   userName: 'Timbobobo',
-  userAvatarURL: 'http://cdn.computerhope.com/hacker.jpg',
-  userTypes: ['Front-end', 'Back-end'],
+  userAvatarURL: 'http://images.wisegeek.com/computer-programmer-or-hacker.jpg',
+  proficiencies: {
+    mobile: 0.4,
+    web: 0.7,
+  },
   projects: [{
     name: 'Solve P = NP',
     description: 'Come join my weekend project!',
@@ -20,21 +24,35 @@ const defaultValues = {
     description: 'project description',
     projectAvatarURL: 'http://www.hdwallpapers.in/walls/windows_xp_bliss-wide.jpg',
     types: ['Front-end', 'Android'],
+  }, {
+    name: 'Project0',
+    description: 'project description',
+    projectAvatarURL: 'http://www.hdwallpapers.in/walls/windows_xp_bliss-wide.jpg',
+    types: ['Front-end', 'Android'],
+  }, {
+    name: 'Project0',
+    description: 'project description',
+    projectAvatarURL: 'http://www.hdwallpapers.in/walls/windows_xp_bliss-wide.jpg',
+    types: ['Front-end', 'Android'],
+  }, {
+    name: 'Project0',
+    description: 'project description',
+    projectAvatarURL: 'http://www.hdwallpapers.in/walls/windows_xp_bliss-wide.jpg',
+    types: ['Front-end', 'Android'],
   }],
-
-
 }
+
 const App = props => (
   <div className="App">
     <Toolbar className="App-toolbar">
       <ToolbarGroup firstChild>
+        <Logo />
       </ToolbarGroup>
       <ToolbarGroup lastChild>
         <FlatButton
           label="Sign out"
           onClick={
             () => {
-              console.log(props)
               props.route.auth.logout()
               browserHistory.push('/login')
             }
@@ -42,7 +60,7 @@ const App = props => (
         />
       </ToolbarGroup>
     </Toolbar>
-    <div className="Content">
+    <div className="Content Side-space">
       <UserInfo />
       <ProjectList />
     </div>
@@ -51,45 +69,77 @@ const App = props => (
 
 const UserInfo = props => (
   <div className="User-info">
-    <div className="User-info-profile-section">
+    <Paper zDepth={3} className="Breathing-room User-info-profile-section">
       <Avatar className="User-info-avatar" src={defaultValues.userAvatarURL} />
       <h1>{defaultValues.firstName} {defaultValues.lastName}</h1>
       <h2>{defaultValues.userName}</h2>
-    </div>
-    <div className="User-info-types-section">
-      <div className="User-info-types-box">
-        {
-          defaultValues.userTypes.map(type => (
-            <p>{type}</p> // todo: use seperate component
-          ))
-        }
-      </div>
-    </div>
+    </Paper>
+    <Paper zDepth={3} className="Breathing-room User-info-types-section">
+      <Doughnut
+        data={{
+          labels: [
+            'Front end web',
+            'Back end web',
+            'Android',
+            'IOS',
+            'Software systems',
+            'Games',
+          ],
+          datasets: [{
+            data: [300, 50, 100, 70, 20, 100],
+            backgroundColor: [
+              '#f7cc0c',
+              '#f70c0c',
+              '#0cf727',
+              '#0cb4f7',
+              '#9e9e9e',
+              '#f7950c',
+            ],
+            hoverBackgroundColor: [
+              '#f7cc0c',
+              '#f70c0c',
+              '#0cf727',
+              '#0cb4f7',
+              '#9e9e9e',
+              '#f7950c',
+            ],
+          }],
+        }}
+      />
+    </Paper>
+  </div>
+)
+
+const ProficiencyGraph = props => (
+  <div className="Bar-graph-container">
+    {JSON.stringify(props.proficiencies)}
   </div>
 )
 
 const ProjectList = props => (
-  <List>
-    <Subheader>Projects you can join</Subheader>
-    {
-      defaultValues.projects.map(project => (
-        <ListItem
-          primaryText={project.name}
-          secondaryText={
-            <div style={{ display: 'Block' }}>
-              <p>project.description</p>
-              {
-                project.types.map(type => (
-                  <p>{type}</p> // todo: use seperate component
-                ))
-              }
-            </div>
-          }
-          leftAvatar={<Avatar src={project.projectAvatarURL} />}
-        />
-      ))
-    }
-  </List>
+  <Paper zDepth={3} className="Breathing-room Projects-info">
+    <List>
+      <Subheader>Projects recommended for you</Subheader>
+      {
+        defaultValues.projects.map(project => (
+          <ListItem
+            primaryText={project.name}
+            secondaryText={
+              <div style={{ display: 'Block' }}>
+                <p>project.description</p>
+                {
+                  project.types.map(type => (
+                    <p>{type}</p> // todo: use seperate component
+                  ))
+                }
+              </div>
+            }
+            leftAvatar={<Avatar src={project.projectAvatarURL} />}
+          />
+        ))
+      }
+    </List>
+  </Paper>
 )
 
 const ProjectListElement = props => (
@@ -97,36 +147,3 @@ const ProjectListElement = props => (
 )
 
 export default App
-
-
-// const App = props => (
-//   <div className="App">
-//     <div className="App-header">
-//       <img src={logo} className="App-logo" alt="logo" />
-//       <h2>Welcome to React</h2>
-//     </div>
-//     <p className="App-intro">
-//       To get started, edit <code>src/App.js</code> and save to reload.
-//     </p>
-//     <div><RaisedButton label="Console log Redux!" onClick={props.loadDefault} /></div>
-//     <div><RaisedButton label="Material UI" /></div>
-//     <div><RaisedButton label="onClick Query" onClick={debouncedQuery} /></div>
-//     <p><Link to="/auth">Auth</Link></p>
-//     <div>
-//       Display Graphql query:&nbsp;
-//       {
-//         props.data.loading || props.data.error
-//         ? <span>Loading...</span>
-//         : (
-//           <div>
-//             {
-//               props.data.allKappachinoes.map(user => (
-//                 <div key={user.id}>{user.posts}</div>
-//               ))
-//             }
-//           </div>
-//         )
-//       }
-//     </div>
-//   </div>
-// )
---
+++ src/components/sharedComponents/logo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './sharedComponents.css'
+
+const Logo = props => (
+  <div style={props.style || {}} className={'Logo'}>Hackr_matchr</div>
+)
+export default Logo
