b774bf71a332ce8e94424e2903023dfb617cf819
Andrew Song <andrewgnos@gmail.com>
2017-03-19 11:52:48 -0700

    hosted url

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,5 @@
-const env = require('./env')
-
-const redirectUrl = env === 'development' ? 'http://localhost:9000/login' : 'https://hackr-matchr.herokuapp.com/login'
-
 module.exports = {
   clientId: 'pXa0STnfkTwO65qGMAWCUxCZJ3mtwu9G',
   domainId: 'sonar-dev.auth0.com',
-  redirectUrl,
+  redirectUrl: 'https://hackr-matchr.herokuapp.com/login',
 }
